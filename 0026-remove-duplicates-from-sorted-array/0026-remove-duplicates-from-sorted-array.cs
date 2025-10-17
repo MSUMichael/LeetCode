75651577aa235e5faadaa76dde63b3a1957a255f
Michael Anglemier <yr9608rk@go.minnstate.edu>
2025-10-17 15:58:43 -0500

    Time: 0 ms (100%), Space: 51.1 MB (57.12%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -3,23 +3,13 @@ public class Solution {
         
         if (nums.Length>0){
             int unique_elements = 1;
-            int[] new_nums;
-            new_nums = new int[nums.Length];
-
-            new_nums[0] = nums[0];
-
-            
             
             for (int i = 0; i < nums.Length-1; i++){
 
                 if (nums[i] != nums[i+1]){
-                    new_nums[unique_elements] = nums[i+1];
+                    nums[unique_elements] = nums[i+1];
                     unique_elements += 1;
-
                 }
-            }
-            for (int i = 0; i < new_nums.Length; i++){
-                nums[i] = new_nums[i];
             }
             
             return unique_elements;
