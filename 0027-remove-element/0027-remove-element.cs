3be5a0490e5fe2026e81353b4c611471692a9b3c
Michael Anglemier <yr9608rk@go.minnstate.edu>
2025-10-17 14:27:54 -0500

    Time: 0 ms (100%), Space: 46.5 MB (95.44%) - LeetHub

--- 0027-remove-element/0027-remove-element.cs
+++ 0027-remove-element/0027-remove-element.cs
@@ -1,24 +1,17 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        
-        if (nums.Length>0){
-            int replace_elem = nums.Max()+1;
-            int num_of_vals = 0; 
-
+        if (nums.Length != 0){
+            int tracker = 0;
             for (int i = 0; i < nums.Length; i++){
-                if (nums[i] == val) {
-                    nums[i] = replace_elem;
-                    num_of_vals += 1;
+                if (nums[i] != val){
+                    nums[tracker] = nums[i];
+                    tracker += 1;
                 }
             }
-            Array.Sort(nums);
-            return nums.Length-num_of_vals;
+            return tracker;
         }
         else {
             return 0;
         }
-        
-        
-
     }
 }
