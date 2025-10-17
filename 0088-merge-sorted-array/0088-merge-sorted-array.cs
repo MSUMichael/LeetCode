8507a057a357c6335ac992a8a0f274321c61b1f8
Michael Anglemier <yr9608rk@go.minnstate.edu>
2025-10-17 15:46:12 -0500

    Time: 0 ms (100%), Space: 46.8 MB (90.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cs
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -1,11 +1,29 @@
 public class Solution {
-
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0;
-        for (i=m; i<nums1.Length; i++){
-            nums1[i] = nums2[i-m];
-        };
-        Array.Sort(nums1);
-    }
+        
+        m -= 1;
+        n -= 1;
 
+        int i = nums1.Length-1;
+
+        while (m >= 0 && n >= 0){
+            if (nums1[m] >= nums2[n]){
+                nums1[i] = nums1[m];
+                m -= 1;
+                i -= 1;
+            }
+            else if (nums1[m] < nums2[n]){
+                nums1[i] = nums2[n];
+                n -= 1;
+                i -= 1;
+            }
+        }
+        
+        while (n >= 0){
+            nums1[i] = nums2[n];
+            n -= 1;
+            i -= 1;
+        }
+
+    }
 }
